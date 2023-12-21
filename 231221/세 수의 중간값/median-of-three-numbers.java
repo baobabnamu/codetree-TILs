477e3d3c47bb334cbcacc5284cf2b069d7525da7
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2023-12-21 16:19:17 +0900

    Title: 세 수의 중간값, Time: 170ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231221/세 수의 중간값/median-of-three-numbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int result = b > a && b < c ? 1 : 0;
+        System.out.print(result);
+    }
+}
