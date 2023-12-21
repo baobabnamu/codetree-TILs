b260db260cf4277b8f84abe7be03c258245fd12a
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2023-12-21 16:12:19 +0900

    Title: 두 번의 연산, Time: 94ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231221/두 번의 연산/two-operations.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        
+        if(a%2==1) {
+            a += 3;
+            if(a%3==0) {
+                a /= 3;
+            }
+        }
+        
+        System.out.printf("%d", a);
+    }
+}
