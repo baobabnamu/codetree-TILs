7fb9f9324b64a10eb3b9f329dc70cd227bca1cab
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2023-12-22 20:40:33 +0900

    Title: 조건에 따른 연산, Time: 91ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 231222/조건에 따른 연산/operatino-by-rule.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int cnt = 1;
+
+        for(int i=1; n<1000; i++) {
+            cnt++;
+            if(n%2 == 0) {
+                n*=3+1;
+            } else {
+                n*=2+2;
+            }
+        }
+        
+        System.out.printf("%d", cnt);
+    }
+}
