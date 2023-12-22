f8082a1a059fb1a0a9f231e69dd2cbc1e147874a
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2023-12-22 20:21:53 +0900

    Title: 1까지 나누기, Time: 96ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 231222/1까지 나누기/divide-by-1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int cnt = 0;
+
+        while(n>=1) {
+            cnt++;
+            n/=cnt;
+        }
+
+        System.out.printf("%d", cnt);
+    }
+}
