8d35fdf55a5bd9da21d8333e63125f3e860774b8
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2023-12-23 22:21:23 +0900

    Title: 알파벳 출력하기 2, Time: 96ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 231223/알파벳 출력하기 2/print-alphabet-2.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        // 아이디어 : 아스키 코드 순회 활용 -> 'A' 65
+        int ascii = 65;
+        for(int i=0; i<n; i++) {
+            if(i>=1) {
+                for(int j=1; j<=i; j++) {
+                    System.out.print("  ");
+                }
+            }
+            for(int j=i; j<n; j++) {
+                System.out.printf("%c ", (char)ascii++);
+            }
+            System.out.println();
+        }
+    }
+}
