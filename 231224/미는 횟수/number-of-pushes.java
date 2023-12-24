c34a90344b87135639f1db06d8a5294fc718fd44
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2023-12-24 16:52:34 +0900

    Title: 미는 횟수, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

--- 231224/미는 횟수/number-of-pushes.java
+++ 231224/미는 횟수/number-of-pushes.java
@@ -4,8 +4,8 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String baseStr = br.readLine();
         StringBuilder sb = new StringBuilder(br.readLine());
+        String baseStr = br.readLine();
         int cnt = 0;
         String temp;
 
