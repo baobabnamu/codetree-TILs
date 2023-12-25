044c5698f6809660f2018604bdccd7ef56545072
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2023-12-25 15:34:58 +0900

    Title: 함수를 이용한 소수 판별, Time: 100ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231225/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static boolean isPrime(int n) {
+        for(int i=2; i<n; i++) {
+            if(n%i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int calcSum(int a, int b) {
+        int sum = 0;
+        for(int i=a; i<=b; i++) {
+            if(isPrime(i)) {
+                sum += i;
+            } 
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int sum = calcSum(a, b);
+        System.out.print(sum);
+    }
+}
