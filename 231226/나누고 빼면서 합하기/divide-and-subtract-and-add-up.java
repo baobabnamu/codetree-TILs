0d8735e0269c905d8c711b99b91e3d52dd766c37
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2023-12-26 07:39:14 +0900

    Title: 나누고 빼면서 합하기, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231226/나누고 빼면서 합하기/divide-and-subtract-and-add-up.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int[] arr;
+
+    public static int magicCalc(int m) {
+        int result = 0;
+
+        while(m != 1) {
+            if(m%2 == 1) {
+                m -= 1;
+            } else {
+                m /= 2;
+            }
+            result += arr[m-1];
+        }
+
+        return result+arr[0];
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        int temp;
+        for(int i=0; i<n; i++) {
+            temp = Integer.parseInt(st.nextToken());
+            arr[i] = temp;
+        }
+
+        int result = magicCalc(m);
+        System.out.printf("%d", result);
+    }
+}
