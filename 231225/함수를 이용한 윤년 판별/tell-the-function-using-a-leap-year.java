744d3d95e4e43a4bd3bc54c9d0b28bd3ab367a81
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2023-12-25 15:43:44 +0900

    Title: 함수를 이용한 윤년 판별, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231225/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static boolean checkLeapYear(int year) {
+        if(year % 4 == 0) {
+            if(year % 100 == 0) {
+                if(year % 400 == 0) {
+                    return true;
+                }
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int year = Integer.parseInt(st.nextToken());
+        System.out.print(checkLeapYear(year));
+    }
+}
