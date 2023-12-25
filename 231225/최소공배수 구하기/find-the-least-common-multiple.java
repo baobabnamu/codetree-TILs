477b3ee9b94e314a365e2edaaa966b543e0be0bb
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2023-12-25 15:03:54 +0900

    Title: 최소공배수 구하기, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

--- 231225/최소공배수 구하기/find-the-least-common-multiple.java
+++ 231225/최소공배수 구하기/find-the-least-common-multiple.java
@@ -1,11 +1,13 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
+
     public static int calcGCD(int n, int m) {
+        // gcd = gcd(n, n%m);
         while(m != 0) {
             int temp = m;
-            m = n % m;
+            m = n%m;
             n = temp;
         }
         return Math.abs(n);
@@ -13,7 +15,7 @@ public class Main {
 
     public static void calcLCM(int n, int m) {
         int gcd = calcGCD(n, m);
-        int lcm = Math.abs(n * m) / gcd;
+        int lcm = Math.abs(n*m)/gcd;
         System.out.print(lcm);
     }
 
