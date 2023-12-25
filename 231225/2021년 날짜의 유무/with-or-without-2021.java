6f4ddddf235c3ec2f8e0ced22ace05071effdf8b
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2023-12-25 20:30:46 +0900

    Title: 2021년 날짜의 유무, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231225/2021년 날짜의 유무/with-or-without-2021.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static boolean checkMonth(int m, int d) {
+        int[] months = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        if(months[m] >= d) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        if(checkMonth(m, d)) {
+            System.out.print("Yes");
+        } else {
+            System.out.print("No");
+        }
+    }
+}
