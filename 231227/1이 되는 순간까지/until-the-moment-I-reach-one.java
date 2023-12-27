e7510e538dc528590d85fb6b0032b40cfb6ce152
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2023-12-27 20:43:49 +0900

    Title: 1이 되는 순간까지, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231227/1이 되는 순간까지/until-the-moment-I-reach-one.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int recursive(int depth, int n) {
+        if(n <= 1) {
+            return depth;
+        }
+
+        if(n % 2 == 0) {
+            return recursive(depth+1, n/2);
+        } 
+
+        return recursive(depth+1, n/3);
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.print(recursive(0, n));
+    }
+}
