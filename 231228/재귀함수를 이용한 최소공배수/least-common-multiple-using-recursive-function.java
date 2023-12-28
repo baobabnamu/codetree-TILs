f46663d99964682d6491959564161ec8c28ac0b2
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2023-12-28 23:06:17 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231228/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int[] arr;
+
+    public static int calcGCD(int n, int m) {
+        while(m != 0) {
+            int temp = m;
+            m = n%m;
+            n = temp;
+        }
+
+        return Math.abs(n);
+    }
+
+    public static int calcLCM(int n, int m) {
+        int gcd = calcGCD(n, m);
+        int lcm = Math.abs(n*m)/gcd;
+        return lcm;
+    }
+
+    public static int magicCalc(int idx, int lcm) {
+        if(idx < 0) {
+            return lcm;
+        }
+        
+        lcm = calcLCM(arr[idx], lcm);
+        return magicCalc(idx - 1, lcm);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++) {
+            int temp = Integer.parseInt(st.nextToken());
+            arr[i] = temp;
+        }
+
+        int lcm = magicCalc(n - 1, 1);
+        System.out.print(lcm);
+    }
+}
