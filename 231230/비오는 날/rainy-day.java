36a118a41046da2f6027fe654ef77157bf353935
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2023-12-30 16:50:50 +0900

    Title: 비오는 날, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

--- 231230/비오는 날/rainy-day.java
+++ 231230/비오는 날/rainy-day.java
@@ -1,72 +1,48 @@
 import java.io.*;
 import java.util.*;
 
-class WeatherData {
-    String date;
-    String day;
-    String weather;
+class Data implements Comparable<Data> {
+    String day, date, weather;
 
-    public WeatherData() {
-        this.date = "";
-        this.day = "";
-        this.weather = "";
+    public Data(String day, String date, String weather) {
+        this.day = day;
+        this.date = date;
+        this.weather = weather;
     }
 
-    public WeatherData(String date, String day, String weather) {
-        this.date = date;
-        this.day = day;
-        this.weather = weather;
+    @Override
+    public int compareTo(Data data) {
+        String[] str1 = this.day.split("-");
+        String[] str2 = data.day.split("-");
+
+        if(str1[0].equals(str2[0])) {
+            if(str1[1].equals(str2[0])) {
+                return Integer.parseInt(str1[2]) - Integer.parseInt(str2[2]);
+            }
+            return Integer.parseInt(str1[1]) - Integer.parseInt(str2[1]);
+        }
+        return Integer.parseInt(str1[0]) - Integer.parseInt(str2[0]);
     }
 }
 
 public class Main {
-
-    public static int[] getRainIndexes(WeatherData[] datas) {
-        ArrayList<Integer> indexes = new ArrayList<>();
-        for(int i=0; i<datas.length; i++) {
-            if(datas[i].weather.equals("Rain")) {
-                indexes.add(i);
-            }
-        }        
-
-        return indexes.stream().mapToInt(x->x).toArray();
-    }
-
-    public static WeatherData getRecentlyRainDay(int[] rainIdxs, WeatherData[] datas) {
-        int answerIdx = 0;
-
-        if(rainIdxs.length == 1) {
-            return datas[rainIdxs[0]];
-        } else {
-            for(int i=1; i<rainIdxs.length; i++) { 
-                int curIdx = rainIdxs[i];
-                int compareIdx = answerIdx;
-                if(datas[curIdx].date.compareTo(datas[compareIdx].date) < 0 ) { // true면 첫 번째 인자가 가까운 시일
-                    answerIdx = curIdx;
-                }
-            }
-        }
-
-        return datas[answerIdx];
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        WeatherData[] datas = new WeatherData[n];
-        
+        int n = Integer.parseInt(br.readLine());
+        Data[] datas = new Data[n];
+
         for(int i=0; i<n; i++) {
-            st = new StringTokenizer(br.readLine());
-            String date = st.nextToken();
-            String day = st.nextToken();
-            String weather = st.nextToken();
-            datas[i] = new WeatherData(date, day, weather);
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            datas[i] = new Data(st.nextToken(), st.nextToken(), st.nextToken());
         }
 
-        // rain인 애들을 저장하는 인덱스 배열 생성 > 제일 근시일 저장 > 선형 탐색
-        int[] rainIdxs = getRainIndexes(datas);
-        WeatherData recentlyRainDayData = getRecentlyRainDay(rainIdxs, datas);
-        System.out.printf("%s %s %s", recentlyRainDayData.date, recentlyRainDayData.day, recentlyRainDayData.weather);
+        Arrays.sort(datas);
+
+        for(Data data : datas) {
+            if(data.weather.equals("Rain")) {
+                System.out.printf("%s %s %s", data.day, data.date, data.weather);
+                break;
+            }
+        }
     }
 }
