1bf69ac6749ec85497a8ef7e1a49f3838e876a1a
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-01-02 20:51:53 +0900

    Title: 왔다 갔던 구역 2, Time: 101ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240102/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 240102/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] lines = new int [2001]; // lines[1000] = 0
+        int[] lines = new int[2001];
         int cur = 1000;
 
         for(int i=0; i<n; i++) {
@@ -13,28 +13,30 @@ public class Main {
             int x = Integer.parseInt(stk.nextToken());
             char c = stk.nextToken().charAt(0);
             if(c == 'R') {
-                int end = cur+x;
-                for(int j=cur; j<end; j++) {
-                    lines[cur++] += 1;
-                    // System.out.println("x : " + x + ", cur : " + cur + ", c : " + c);
+                for(int j=cur; j<=cur+x; j++) {
+                    lines[j] += 1;
                 }
+                cur = cur+x;
             } else {
-                int end = cur-x;
-                for(int j=cur; cur>end; j--) {
-                    lines[cur--] += 1;
-                    // System.out.println("x : " + x + ", cur : " + cur + ", c : " + c);
+                for(int j=cur-x; j<=cur; j++) {
+                    lines[j] += 1;
                 }
+                cur = cur-x;
             }
         }
 
-        Arrays.sort(lines);
 
+        int sum = 0;
         int cnt = 0;
-        for(int i=lines.length-1; i>=0; i--) {
+        for(int i=0; i<lines.length-1; i++) {
             if(lines[i] >= 2) {
                 cnt++;
+            } else if(cnt >= 2 && lines[i] < 2) {
+                sum += cnt - 1;
+                cnt = 0;
             }
         }
-        System.out.print(cnt);
+
+        System.out.print(sum);
     }
 }
