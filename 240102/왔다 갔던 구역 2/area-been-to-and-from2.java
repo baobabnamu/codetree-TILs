80da2cf6f4ef2e67c9b9d2dc61b8434778277eea
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-01-02 22:26:10 +0900

    Title: 왔다 갔던 구역 2, Time: 163ms, Memory: 8MB, Status: Passed - CodeTree

--- 240102/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 240102/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -2,41 +2,52 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static int offset = 1000;
+    public static int[] lines = new int[2001];
+
+    public static int count() {
+        int cnt = 0;
+        for(int i=0; i<lines.length - 1; i++) {
+            if(lines[i] >= 2) {
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+
+    public static void location(int start, int end) {
+        start += offset;
+        end += offset;
+        for(int i=start; i<end; i++) {
+            lines[i] += 1;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] lines = new int[2001];
-        int cur = 1000;
+        int initPoint = 0;
 
         for(int i=0; i<n; i++) {
             StringTokenizer stk = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(stk.nextToken());
-            char c = stk.nextToken().charAt(0);
-            if(c == 'R') {
-                for(int j=cur; j<=cur+x; j++) {
-                    lines[j] += 1;
-                }
-                cur = cur+x;
+            int move = Integer.parseInt(stk.nextToken());
+            String direction = stk.nextToken();
+
+            int leftPoint = initPoint;
+            int rightPoint = initPoint;
+
+            if(direction.equals("L")) {
+                leftPoint -= move;
+                initPoint -= move;
             } else {
-                for(int j=cur-x; j<=cur; j++) {
-                    lines[j] += 1;
-                }
-                cur = cur-x;
+                rightPoint += move;
+                initPoint += move;
             }
+
+            location(leftPoint, rightPoint);
         }
 
-
-        int sum = 0;
-        int cnt = 0;
-        for(int i=0; i<lines.length-1; i++) {
-            if(lines[i] >= 2) {
-                cnt++;
-            } else if(cnt >= 2 && lines[i] < 2) {
-                sum += cnt - 1;
-                cnt = 0;
-            }
-        }
-
-        System.out.print(sum);
+        System.out.print(count());
     }
 }
