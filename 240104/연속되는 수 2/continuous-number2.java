597f805a4d45f4190cdc52cbaa55c316473d36ac
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-01-04 20:07:05 +0900

    Title: 연속되는 수 2, Time: 167ms, Memory: 9MB, Status: Passed - CodeTree

--- 240104/연속되는 수 2/continuous-number2.java
+++ 240104/연속되는 수 2/continuous-number2.java
@@ -11,11 +11,13 @@ public class Main {
             nums[i] = Integer.parseInt(br.readLine());
 
         int cnt = 0;
-        int maxCnt = 1;
-        for(int i=0; i<n; i++)
+        int ans = 1;
+        for(int i=0; i<n; i++) {
             if(i == 0 || nums[i] == nums[i-1]) cnt++;
-
-        maxCnt = Math.max(cnt, maxCnt);
-        System.out.print(maxCnt);
+            else if(nums[i] != nums[i-1]) cnt = 1;
+            ans = Math.max(cnt, ans);
+        }
+            
+        System.out.print(ans);
     }
 }
