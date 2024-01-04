236475b8eba818012f8cb703edabfe270b601e5d
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-01-04 20:21:45 +0900

    Title: 연속되는 수 4, Time: 96ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240104/연속되는 수 4/continuous-number4.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] nums = new int[n];
+
+        for(int i=0; i<n; i++) nums[i] = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+        int ans = 1;
+        for(int i=0; i<n; i++) {
+            if(i == 0 || nums[i] > nums[i - 1]) cnt++;
+            else cnt = 1;
+
+            ans = Math.max(cnt, ans);
+        }
+
+        System.out.print(ans);
+    }
+}
