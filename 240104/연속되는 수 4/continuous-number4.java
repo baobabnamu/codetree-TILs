c361e255c22257672c9ceefe164cd64eae3cf18b
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-01-04 20:22:14 +0900

    Title: 연속되는 수 4, Time: 111ms, Memory: 9MB, Status: Passed - CodeTree

--- 240104/연속되는 수 4/continuous-number4.java
+++ 240104/연속되는 수 4/continuous-number4.java
@@ -9,10 +9,10 @@ public class Main {
 
         for(int i=0; i<n; i++) nums[i] = Integer.parseInt(br.readLine());
 
-        int cnt = 0;
+        int cnt = 1;
         int ans = 1;
-        for(int i=0; i<n; i++) {
-            if(i == 0 || nums[i] > nums[i - 1]) cnt++;
+        for(int i=1; i<n; i++) {
+            if(nums[i] > nums[i - 1]) cnt++;
             else cnt = 1;
 
             ans = Math.max(cnt, ans);
