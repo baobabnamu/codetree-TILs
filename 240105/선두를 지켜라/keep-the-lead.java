0e5e5cb5b80e3fb40f06f6b7ebded445e65c02c0
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-01-05 10:38:09 +0900

    Title: 선두를 지켜라, Time: 148ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240105/선두를 지켜라/keep-the-lead.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static final int MAX_R = 1000000 + 1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(stk.nextToken());
+        int m = Integer.parseInt(stk.nextToken());
+
+        int[] aLog = new int[MAX_R];
+        int[] bLog = new int[MAX_R];
+
+        int aCur = 0;
+        int bCur = 0;
+        int aIdx = 0;
+        int bIdx = 0;
+
+        // 1. A, B 시뮬레이션을 통해 배열에 기록
+        for(int i=0; i<n; i++) {
+            stk = new StringTokenizer(br.readLine());
+            int v = Integer.parseInt(stk.nextToken());
+            int t = Integer.parseInt(stk.nextToken());
+
+            while(t-- > 0) {
+                aCur += v;
+                aLog[++aIdx] = aCur;
+            }
+        }
+
+        for(int i=0; i<m; i++) {
+            stk = new StringTokenizer(br.readLine());
+            int v = Integer.parseInt(stk.nextToken());
+            int t = Integer.parseInt(stk.nextToken());
+
+            while(t-- > 0) {
+                bCur += v;
+                bLog[++bIdx] = bCur;
+            }
+        }       
+        
+        // 2. 선두 확인
+        int ans = -1;
+        int[] firstLog = new int[MAX_R];
+        
+        for(int i=1; i<MAX_R; i++) {
+            // 선두 체킹
+            if(aLog[i] > bLog[i]) firstLog[i] = 1;
+            else if(aLog[i] < bLog[i]) firstLog[i] = 2;
+            else firstLog[i] = firstLog[i-1];
+
+            // 선두가 이전과 다른 경우 ans++;
+            if(firstLog[i] != firstLog[i-1]) ans++;
+        }
+
+        System.out.print(ans);
+    }
+}
