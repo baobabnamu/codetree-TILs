d04a3ea187993ce9faf76bd306a425771a557384
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-01-05 11:18:56 +0900

    Title: 좌우로 움직이는 로봇, Time: 139ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 240105/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
+++ 240105/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -9,6 +9,7 @@ public class Main {
         // 주의 : 바로 직전에 같은 위치에 있었다면 경우의 수로 포함하지 않음.
         // 주의 : 원점을 기준으로 시작하며 이는 횟수에 포함하지 않음. 이후, 원점에서 다시 이동하는 게 아니라 마지막 위치를 기준으로 이동함.
         // 주의 : 로봇의 총 합 거리는 100만 이하임.
+        // 주의 : 로봇 간 이동 시간이 다를 수 있으며, 어떠한 로봇이 멈췄다고 해서 다른 로봇이 멈추는 건 아님.
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stk = new StringTokenizer(br.readLine());
@@ -33,10 +34,13 @@ public class Main {
             }
         }
 
+        // 주의 : 로봇이 멈춘 경우, 그 뒤에 모든 위치는 마지막 로봇 위치.
+        for(int i=aIdx; i<1000; i++) aLog[i] = aLog[aIdx]; 
+
         int[] bLog = new int[MAX_R];
         int bIdx = 0;
 
-        for(int i=0; i<n; i++) {
+        for(int i=0; i<m; i++) {
             stk = new StringTokenizer(br.readLine());
             int t = Integer.parseInt(stk.nextToken());
             char d = stk.nextToken().charAt(0);
@@ -50,12 +54,15 @@ public class Main {
             }
         }
 
+        // 주의 : 마지막 로봇 위치로 변경
+        for(int i=bIdx; i<MAX_R; i++) bLog[i] = bLog[bIdx]; 
+
         // 2. A, B 순회하면서 조건에 맞는 경우의 수 카운팅
         // 조건 : A-1 != B-1 && A == B
         int ans = 0;
         for(int i=1; i<MAX_R; i++) {
             if(aLog[i-1] != bLog[i-1] && aLog[i] == bLog[i]) {
-              ans++;  
+                ans++;
             }
         }
 
