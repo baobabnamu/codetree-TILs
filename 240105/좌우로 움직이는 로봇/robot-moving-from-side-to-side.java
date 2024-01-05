ac9411c55f0fc54128cae7eee76c8dcad81b12f8
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-01-05 10:59:59 +0900

    Title: 좌우로 움직이는 로봇, Time: 108ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 240105/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static final int MAX_R = 1000000;
+
+    public static void main(String[] args) throws IOException {
+        // 로봇 A와 B의 위치가 바로 직전에는 다른 위치에 있다가 그 다음번에 같은 위치에 오게되는 경우의 수
+        // 주의 : 바로 직전에 같은 위치에 있었다면 경우의 수로 포함하지 않음.
+        // 주의 : 원점을 기준으로 시작하며 이는 횟수에 포함하지 않음. 이후, 원점에서 다시 이동하는 게 아니라 마지막 위치를 기준으로 이동함.
+        // 주의 : 로봇의 총 합 거리는 100만 이하임.
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(stk.nextToken());
+        int m = Integer.parseInt(stk.nextToken());
+
+        // 1. A, B 기록
+        int[] aLog = new int[MAX_R];
+        int aIdx = 0;
+
+        for(int i=0; i<n; i++) {
+            stk = new StringTokenizer(br.readLine());
+            int t = Integer.parseInt(stk.nextToken());
+            char d = stk.nextToken().charAt(0);
+
+            while(t-- > 0) {
+                if(d == 'L') {
+                    aLog[++aIdx] = aLog[aIdx-1] - 1;
+                } else {
+                    aLog[++aIdx] = aLog[aIdx-1] + 1;
+                }
+            }
+        }
+
+        int[] bLog = new int[MAX_R];
+        int bIdx = 0;
+
+        for(int i=0; i<n; i++) {
+            stk = new StringTokenizer(br.readLine());
+            int t = Integer.parseInt(stk.nextToken());
+            char d = stk.nextToken().charAt(0);
+
+            while(t-- > 0) {
+                if(d == 'L') {
+                    bLog[++bIdx] = bLog[bIdx-1] - 1;
+                } else {
+                    bLog[++bIdx] = bLog[bIdx-1] + 1;
+                }
+            }
+        }
+
+        // 2. A, B 순회하면서 조건에 맞는 경우의 수 카운팅
+        // 조건 : A-1 != B-1 && A == B
+        int ans = 0;
+        for(int i=1; i<MAX_R; i++) {
+            if(aLog[i-1] != bLog[i-1] && aLog[i] == bLog[i]) {
+              ans++;  
+            }
+        }
+
+        System.out.print(ans);
+    }
+}
