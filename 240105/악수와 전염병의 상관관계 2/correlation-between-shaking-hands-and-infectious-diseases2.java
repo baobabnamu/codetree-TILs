0984706411697c14ca5bc68e869baafdfb342bed
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-01-05 14:25:58 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 118ms, Memory: 9MB, Status: Passed - CodeTree

--- 240105/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
+++ 240105/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
@@ -1,82 +1,92 @@
-import java.util.Scanner;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
-// 악수의 정보를 나타내는 클래스 선언
-class Shake implements Comparable<Shake> {
-    int time;
-    int person1;
-    int person2;
-    
-    public Shake(int time, int person1, int person2) {
-        this.time = time;
-        this.person1 = person1;
-        this.person2 = person2;
-    }
+class Person {
+    // sick : 전염 1, 미전염 0
+    int count, sick;
 
-    @Override
-    public int compareTo(Shake shake) {
-        // 시간을 기준으로 오름차순으로 정렬합니다.
-        return time - shake.time;
+    Person() {}
+
+    public Person(int count, int sick) {
+        this.count = count;
+        this.sick = sick;
     }
-};
+}
 
 public class Main {
-    public static final int MAX_T = 250;
     public static final int MAX_N = 100;
-    
-    public static int n, k, p, t;
-    public static int[] shakeNum = new int[MAX_N + 1];
-    public static boolean[] infected = new boolean[MAX_N + 1];
+    public static final int MAX_K = 250;
+    public static final int MAX_THRESHOLD = 250;
+    public static final int MAX_TIME = 250;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // 입력
-        n = sc.nextInt();
-        k = sc.nextInt();
-        p = sc.nextInt();
-        t = sc.nextInt();
-        infected[p] = true;
+    public static void main(String[] args) throws IOException {
+        // 최종적으로 어떤 사람들이 전염병에 걸려있는가?
+        // 주의 : 전염된 사람끼리 만나도 전염시킨 것으로 간주함
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(stk.nextToken()); // 개발자 수
+        int K = Integer.parseInt(stk.nextToken()); // 전염병 임계값 (K번의 악수)
+        int P = Integer.parseInt(stk.nextToken()); // 처음 전염병 걸린 사람
+        int T = Integer.parseInt(stk.nextToken()); // 정황의 횟수
+
+        // 1. 사람 클래스 (악수 횟수, 감염 여부) 객체 관리
+        // 2. 사람 클래스 배열 생성 후 N만큼 객체 생성
+        Person[] persons = new Person[N + 1];
+        for(int i=1; i<=N; i++)
+            persons[i] = new Person(0, 0);
+
+        // 첫 번째 환자
+        persons[P].sick = 1; 
         
-        Shake[] shakes = new Shake[MAX_T];
-        
-        for(int i = 0; i < t; i++) {
-            int time = sc.nextInt();
-            int person1 = sc.nextInt();
-            int person2 = sc.nextInt();
-            // Shake 객체를 생성해 넣어줍니다.
-            shakes[i] = new Shake(time, person1, person2);
+        // 3. 시간 2차원 배열 (행:시간, 열:X, Y) 어느 시간에 누가 악수했는지 관리
+        int[][] timeTable = new int[MAX_TIME + 1][MAX_TIME + 1];
+        int startTime = MAX_TIME;
+        int endTime = 0;
+        for(int i=0; i<T; i++) {
+            stk = new StringTokenizer(br.readLine());
+            int t = Integer.parseInt(stk.nextToken()); // 시간(초) = 악수한 타이밍
+            int x = Integer.parseInt(stk.nextToken()); // 악수한 x
+            int y = Integer.parseInt(stk.nextToken()); // 악수한 y
+
+            timeTable[t][0] = x;
+            timeTable[t][1] = y;
+
+            startTime = Math.min(startTime, t);
+            endTime = Math.max(endTime, t);
         }
-        
-        // custom comparator를 활용한 정렬
-        Arrays.sort(shakes, 0, t);
-        
-        // 각 악수 횟수를 세서,
-        // K번 초과로 악수를 했을 시 전염시키지 않습니다.
-        for(int i = 0; i < t; i++) {
-            int target1 = shakes[i].person1;
-            int target2 = shakes[i].person2;
-            
-            // 감염되어 있을 경우 악수 횟수를 증가시킵니다.
-            if(infected[target1])
-                shakeNum[target1]++;
-            if(infected[target2])
-                shakeNum[target2]++;
-            
-            // target1이 감염되어 있고 아직 K번 이하로 악수했다면 target2를 전염시킵니다.
-            if(shakeNum[target1] <= k && infected[target1])
-                infected[target2] = true;
-            
-            // target2가 감염되어 있고 아직 K번 이하로 악수했다면 target1을 전염시킵니다.
-            if(shakeNum[target2] <= k && infected[target2])
-                infected[target1] = true;
+
+        // 4. 시간에 맞게 시뮬레이션 진행
+        for(int i=startTime; i<=endTime; i++) {
+            int x = timeTable[i][0];
+            int y = timeTable[i][1];
+
+            if(x==0 || y==0) continue;
+
+
+            // x는 감염되어 있는데 y는 감염 안 되어있는 경우
+            // y는 감염되어 있는데 x는 감염 안 되어있는 경우
+            // 둘 다 감염되어 있는 경우
+            // x, y 둘 다 감염이 안 되어있는 우
+            if(persons[x].sick == 1 && persons[y].sick == 0) {
+                if(persons[x].count < K) {
+                    persons[y].sick = 1;
+                    persons[x].count++;
+                }
+            } else if(persons[x].sick == 0 && persons[y].sick == 1) {
+                if(persons[y].count < K) {
+                    persons[x].sick = 1;
+                    persons[y].count++;
+                }
+            } else if(persons[x].sick == 1 && persons[y].sick == 1) {
+                persons[x].count++;
+                persons[y].count++;
+            }
         }
-        
-        for(int i = 1; i <= n; i++) {
-            if(infected[i])
-                System.out.print(1);
-            else
-                System.out.print(0);
+
+        // 5. 사람 클래스 배열 순회해서 감염여부 반환
+        for(int i=1; i<=N; i++) {
+            System.out.printf("%d", persons[i].sick);
         }
-        
     }
 }
