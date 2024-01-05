392fd245858a5e9bc7fe8c32f2bc26182b2ca339
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-01-05 15:09:24 +0900

    Title: 문자에 따른 명령 2, Time: 106ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240105/문자에 따른 명령 2/text-based-commands2.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String commands = br.readLine();
+        char[] commandsToCharArr = commands.toCharArray();
+
+        int nx = 0;
+        int ny = 0;
+
+        // 시계방향
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {-1, 0, 1, 0};
+
+        int dIdx = 0;
+
+        for(char command : commandsToCharArr) {
+            if(command == 'L') {
+                dIdx = (dIdx - 1 + 4) % 4;
+            } else if (command == 'R') {
+                dIdx = (dIdx + 1) % 4;
+            } else {
+                nx += dx[dIdx];
+                ny += dy[dIdx];
+            }
+        }
+
+        System.out.printf("%d %d", nx, ny);
+    }
+}
