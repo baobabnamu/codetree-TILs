8397ef788e54f1011dc7fa5d2af44ea818a65741
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-01-06 09:53:29 +0900

    Title: 격자 위의 편안한 상태, Time: 256ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 240106/격자 위의 편안한 상태/comfortable-state-on-the-grid.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int n;
+
+    public static boolean inRange(int x, int y) {
+        if(x >= 0 && x < n && y >= 0 && y < n) return true;
+        return false;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(stk.nextToken());
+        int m = Integer.parseInt(stk.nextToken());
+
+        int[][] rect = new int[n][n];
+        int[] dx = {-1, 1, 0, 0}; // U D R L
+        int[] dy = {0, 0, 1, -1};
+
+        for(int i=0; i<m; i++) {
+            stk = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(stk.nextToken()) - 1;
+            int c = Integer.parseInt(stk.nextToken()) - 1;
+            int cnt = 0;
+            int status = 0;
+
+            rect[r][c] = 1;
+            for(int dirNum=0; dirNum<4; dirNum++) {
+                int nx = r + dx[dirNum];
+                int ny = c + dy[dirNum];
+
+                if(inRange(nx, ny) && rect[nx][ny] != 0) {
+                    cnt++;
+                }
+            }
+
+            if(cnt >= 3) status = 1;
+            System.out.printf("%d\n", status);
+        }
+    }
+}
