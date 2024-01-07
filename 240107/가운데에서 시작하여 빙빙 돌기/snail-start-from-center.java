b6d646fb68aee19ed6ce4d1e13fb23d214e55fa3
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-01-07 20:29:47 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 499ms, Memory: 20MB, Status: Passed - CodeTree

--- 240107/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.java
+++ 240107/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.java
@@ -16,18 +16,23 @@ public class Main {
         int x = n / 2, y = n / 2;
         rect[x][y] = 1;
 
-        int value = 2, move = 1;
+        int value = 2, move = 1, moveCnt = 0;
         while(value <= n * n) {
-            for(int i=0; i<2; i++) {
-                for(int j=0; j<move; j++) {
-                    if(value > n * n) break;
-                    x += dx[dirNum];
-                    y += dy[dirNum];
-                    rect[x][y] = value++;
-                }
-                dirNum = (dirNum + 1) % 4; // move 값을 다 사용하면 방향 변경
+            moveCnt++;
+            
+            for(int j=0; j<move; j++) {
+                if(value > n * n) break;
+                x += dx[dirNum];
+                y += dy[dirNum];
+                rect[x][y] = value++;
             }
-            move++;
+
+            dirNum = (dirNum + 1) % 4; // move 값을 다 사용하면 방향 변경
+
+            if(moveCnt >= 2) {
+                move++;
+                moveCnt = 0;
+            }
         }
 
         for(int i=0; i<n; i++) {
