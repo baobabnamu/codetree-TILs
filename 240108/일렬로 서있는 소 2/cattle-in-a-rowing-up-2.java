7223e7148cb5d86954bd82b51822acc0898d214f
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-01-08 11:52:40 +0900

    Title: 일렬로 서있는 소 2, Time: 106ms, Memory: 8MB, Status: Passed - CodeTree

--- 240108/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
+++ 240108/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -15,7 +15,7 @@ public class Main {
         for(int i=0; i<n; i++) {
             for(int j=i+1; j<n; j++) {
                 for(int k=j+1; k<n; k++) {
-                    if(i < j && j < k && cows[i] < cows[j] && cows[j] < cows[k]) {
+                    if(i < j && j < k && cows[i] <= cows[j] && cows[j] <= cows[k]) {
                         cnt++;
                     }
                 }
