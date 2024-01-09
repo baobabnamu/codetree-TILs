c955ae5de0c6aa27cac6f169db5ccf64f66156cc
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-01-09 22:31:46 +0900

    Title: 최고의 13위치 2, Time: 117ms, Memory: 9MB, Status: Passed - CodeTree

--- 240109/최고의 13위치 2/best-place-of-13-2.java
+++ 240109/최고의 13위치 2/best-place-of-13-2.java
@@ -2,26 +2,26 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    private static int n;
+    private static int N;
     private static int[][] grid;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        grid = new int[n][n];
+        N = Integer.parseInt(br.readLine());
+        grid = new int[N][N];
 
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < N; i++) {
             StringTokenizer stk = new StringTokenizer(br.readLine());
-            for(int j = 0; j < n; j++) {
+            for(int j = 0; j < N; j++) {
                 grid[i][j] = Integer.parseInt(stk.nextToken());
             }
         }
 
         int max = 0;
-        for(int x1 = 0; x1 < n; x1++) {
-            for(int y1 = 0; y1 < n - 2; y1++) {
-                for(int x2 = 0; x2 < n; x2++) {
-                    for(int y2 = 0; y2 < n - 2; y2++) {
+        for(int x1 = 0; x1 < N; x1++) {
+            for(int y1 = 0; y1 < N - 2; y1++) {
+                for(int x2 = 0; x2 < N; x2++) {
+                    for(int y2 = 0; y2 < N - 2; y2++) {
                         if(isEqualBox(x1, y1, x2, y2)) continue;
 
                         int box1 = grid[x1][y1] + grid[x1][y1 + 1] + grid[x1][y1 + 2];
@@ -35,9 +35,7 @@ public class Main {
         System.out.print(max);
     }
 
-    public static boolean isEqualBox(int x1, int y1, int x2, int y2) {
-        // 행이 똑같으면서 y1과 y2의 거리가 2이하 일때
-        // 거리 2이하인 기준은 격자의 크기가 3이기 때문
+    private static boolean isEqualBox(int x1, int y1, int x2, int y2) {
         return x1 == x2 && Math.abs(y1 - y2) <= 2;
     }
 }
