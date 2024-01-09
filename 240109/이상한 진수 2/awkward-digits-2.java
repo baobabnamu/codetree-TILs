cd248bc2d61f1e27a5883e9b721c5ef457a661d1
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-01-09 10:10:52 +0900

    Title: 이상한 진수 2, Time: 99ms, Memory: 8MB, Status: Passed - CodeTree

--- 240109/이상한 진수 2/awkward-digits-2.java
+++ 240109/이상한 진수 2/awkward-digits-2.java
@@ -4,37 +4,21 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-        int len = s.length();
-        int[] nums = new int[len];
-        boolean isFind = false;    
+        char[] a = br.readLine().toCharArray();
+        int max = 0;
 
-        for(int i = 0; i < len; i++) {
-            if(!isFind && s.charAt(i) == '0') {
-                nums[i] = 1;
-                isFind = true;
-            } else if(i == len - 1 && !isFind && s.charAt(i) != '0') {
-                nums[i] = 0;
-            } else {
-                nums[i] = s.charAt(i) - 48;
-            }
+        for(int i = 1; i < a.length; i++) {
+            a[i] = toOppositeNum(a[i]); // 비교를 위해 숫자 뒤집고
+            String binaryN = String.valueOf(a);
+            max = Math.max(max, Integer.parseInt(binaryN, 2));
+            a[i] = toOppositeNum(a[i]); // 비교 후 다시 원복
         }
 
-        int ans = 0;
-        int power = 0;
-        for(int i = len - 1; i >= 0; i--) {
-            int num = nums[i]; // 숫자 변환
-            if(num == 1) {
-                ans += Math.pow(2, power);
-            }
-            power++;
-        }
+        System.out.print(max);
+    }
 
-        System.out.print(ans);
-        // 1. 완탐
-        // for문 돌면서 0 만나면 1로 변경
-        // 마지막 인덱스인데도 만약 0을 한 번도 만나지 못했다면 마지막 1를 0으로 변경
-
-        // 2. 2진수로 변환
+    public static char toOppositeNum(char digit) {
+        if(digit == '1') return '0';
+        return '1';
     }
 }
