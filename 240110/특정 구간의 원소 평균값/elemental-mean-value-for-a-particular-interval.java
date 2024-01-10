0a73d663cd0e05f2d2ef86f9da03abed80b077fa
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-01-10 22:38:07 +0900

    Title: 특정 구간의 원소 평균값, Time: 127ms, Memory: 9MB, Status: Passed - CodeTree

--- 240110/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.java
+++ 240110/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.java
@@ -9,47 +9,34 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         nums = new double[N];
 
-        double sum = 0;
         StringTokenizer stk = new StringTokenizer(br.readLine());
         for(int i = 0; i < N; i++) {
             nums[i] = Double.parseDouble(stk.nextToken());
-            sum += nums[i];
         }
         
-        int ans = N;
-
-        double avgTotal = sum / N;
+        int ans = 0;
         for(int i = 0; i < N; i++) {
-            if(avgTotal == nums[i]) {
-                ans++;
-                break;
-            }
-        }
+            for(int j = i; j < N; j++) {
+                // [i, j] 비교
+                double sum = 0;
+                int cnt = 0;
 
-        int k = 2;
-        while(k <= N) {
-            for(int i = 0; i <= N - k; i++) {
-                double avg = calcAvg(i, k);
-                for(int j = i; j < k; j++) {
-                    if(avg == nums[j]) {
+                for(int k = i; k <= j; k++) {
+                    sum += nums[k];
+                    cnt++;
+                }
+
+                double avg = sum / cnt;
+
+                for(int k = i; k <= j; k++) {
+                    if(nums[k] == avg) {
                         ans++;
                         break;
                     }
                 }
             }
-            k++;
         }
 
-
-
         System.out.print(ans);
-    }
-
-    private static double calcAvg(int startPoint, int size) {
-        double sum = 0;
-        for(int i = startPoint; i < size; i++)
-            sum += nums[i];
-
-        return sum / size;
     }
 }
