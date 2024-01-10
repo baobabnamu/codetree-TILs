8141d6ed1bb82058a0d759df229a8e9c0252eb3d
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-01-10 16:29:31 +0900

    Title: G or H 3, Time: 123ms, Memory: 9MB, Status: Passed - CodeTree

--- 240110/G or H 3/G-or-H-3.java
+++ 240110/G or H 3/G-or-H-3.java
@@ -11,27 +11,24 @@ public class Main {
         int N = Integer.parseInt(stk.nextToken());
         int K = Integer.parseInt(stk.nextToken()) + 1; // 사진의 크기는 x 값끼리의 차 -> 예시) 0, 6 -> 0, 1, 2, 3, 4, 5, 6인 총 7개 좌표 탐색
 
+        int len = MAX_L;
         int[] locations = new int[N];
         int[] alphabets = new int[N];
+        int[] lines = new int[len];
 
         for(int i = 0; i < N; i++) {
             stk = new StringTokenizer(br.readLine());
             locations[i] = Integer.parseInt(stk.nextToken());
             char alphabet = stk.nextToken().charAt(0);
             alphabets[i] = alphabet == 'G' ? 1 : 2;
-        }
-
-        int len = MAX_L;
-        int[] lines = new int[len];
-        for(int i = 0; i < locations.length; i++)
             lines[locations[i] - 1] = alphabets[i];
+        } 
 
         int max = INT_MIN;
         for(int i = 0; i <= len - K; i++) {
             int sum = 0;
-            for(int j = i; j < i + K; j++) {
+            for(int j = i; j < i + K; j++)
                 sum += lines[j];
-            }
             max = Math.max(max, sum);
         }
 
