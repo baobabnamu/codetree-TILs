7e070cdfd4954c69bd54d49b7c792cca5768ebd6
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-01-11 12:00:31 +0900

    Title: 개발자의 능력 3, Time: 108ms, Memory: 8MB, Status: Passed - CodeTree

--- 240111/개발자의 능력 3/ability-of-developer-3.java
+++ 240111/개발자의 능력 3/ability-of-developer-3.java
@@ -10,27 +10,19 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        int totalSum = 0;
         for(int i = 0; i < MAX_P; i++) {
             persons[i] = Integer.parseInt(stk.nextToken());
+            totalSum += persons[i];
         }
 
         int min = INT_MAX;
         for(int i = 0; i < MAX_P; i++) {
             for(int j = i + 1; j < MAX_P; j++) {
                 for(int k = j + 1; k < MAX_P; k++) {
-                    int sumA = 0, sumB = 0;
-                    boolean[] visited = new boolean[MAX_P];
-
-                    sumA += persons[i] + persons[j] + persons[k];
-                    visited[i] = true; visited[j] = true; visited[k] = true;
-                    
-                    for(int l = 0; l < MAX_P; l++) {
-                        if(!visited[l]) {
-                            sumB += persons[l];
-                        }
-                    }
-
-                    min = Math.min(min, Math.abs(sumA - sumB));
+                    int sum = persons[i] + persons[j] + persons[k]; // 선택한 3개의 숫자의 합
+                    min = Math.min(min, Math.abs((totalSum - sum) - sum)); // 숫자 6개 전체의 합 - 선택한 3개의 숫자의 합 = 선택하지 않은 숫자 3개의합
                 }
             }
         }
