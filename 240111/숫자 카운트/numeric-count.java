7f3199b7baffa09ead82121154f2861a0f5ff570
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-01-11 18:29:16 +0900

    Title: 숫자 카운트, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240111/숫자 카운트/numeric-count.java
+++ 240111/숫자 카운트/numeric-count.java
@@ -43,6 +43,7 @@ public class Main {
 
                         if(strike != strikes[m] || ball != balls[m]) {
                             isSuccessed = false;
+                            break;
                         }
                     }
 
