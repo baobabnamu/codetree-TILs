628c66303c62e5f5ee63e9ac33015d5350662803
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-01-11 09:58:28 +0900

    Title: G or H 2, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

--- 240111/G or H 2/G-or-H-2.java
+++ 240111/G or H 2/G-or-H-2.java
@@ -7,29 +7,29 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] lines = new int[MAX_L + 1];
+        int[] line = new int[MAX_L + 1];
 
         for(int i = 0; i < N; i++) {
             StringTokenizer stk = new StringTokenizer(br.readLine());
             int idx = Integer.parseInt(stk.nextToken());
-            lines[idx] = stk.nextToken().charAt(0) == 'G' ? 1 : 2; // G : 1, H : 2
+            line[idx] = stk.nextToken().charAt(0) == 'G' ? 1 : 2;
         }
 
-        int max = 0;
+        int ans = 0;
         for(int i = 0; i <= MAX_L; i++) {
-            if(lines[i] == 0) continue;
-            int hCnt = 0, gCnt = 0;
+            if(line[i] == 0) continue;
+            int G = 0, H = 0;
 
             for(int j = i; j <= MAX_L; j++) {
-                if(lines[j] == 0) continue;
-                else if(lines[j] == 1) gCnt++;
-                else if(lines[j] == 2) hCnt++;
+                if(line[j] == 0) continue;
+                else if(line[j] == 1) G++;
+                else if(line[j] == 2) H++;
 
-                if(gCnt == 0 || hCnt == 0 || gCnt == hCnt)
-                    max = Math.max(max, Math.abs(i - j));
+                if(G == 0 || H == 0 || G == H)
+                    ans = Math.max(ans, Math.abs(i - j));
             }
         }
 
-        System.out.print(max);
+        System.out.print(ans);
     }
 }
