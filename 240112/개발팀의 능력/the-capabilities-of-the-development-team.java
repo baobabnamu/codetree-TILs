a3cdf19a9345fd3b5b07d74a4cea85e0e1383609
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-01-12 23:01:57 +0900

    Title: 개발팀의 능력, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

--- 240112/개발팀의 능력/the-capabilities-of-the-development-team.java
+++ 240112/개발팀의 능력/the-capabilities-of-the-development-team.java
@@ -3,60 +3,41 @@ import java.util.*;
 
 public class Main {
     private static final int INT_MAX = Integer.MAX_VALUE;
+    private static final int MAX_N = 5;
 
-    private static int n = 5;
-    private static int[] persons = new int[n];
-    private static boolean[] visited = new boolean[n];
-
+    private static int n = MAX_N;
+    private static int[] persons = new int[MAX_N];
+    private static int totalSum = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stk = new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i++) {
             persons[i] = Integer.parseInt(stk.nextToken());
+            totalSum += persons[i];
         }
 
         int ans = INT_MAX;
-        boolean isDiff = false;
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
-                if(i == j) continue;
-                int sum1 = persons[i] + persons[j];
-                visited[i] = true;
-                visited[j] = true;
-
                 for(int k = 0; k < n; k++) {
-                    for(int l = 0; l < n; l++) {
-                        if(i == k || i == l || j == k || j == l || k == l) continue;
-                        int sum2 = persons[k] + persons[l];
-                        visited[k] = true;
-                        visited[l] = true;
-
-                        int sum3 = 0;
-                        for(int m = 0; m < n; m++) {
-                            if(!visited[m]) {
-                                sum3 = persons[m];
-                            }
-                        }
-
-                        if(sum1 != sum2 && sum2 != sum3 && sum1 != sum3) {
-                            int[] sums = {sum1, sum2, sum3};
-                            Arrays.sort(sums);
-                            int temp = sums[2] - sums[0];
-                            ans = Math.min(ans, temp);
-                            isDiff = true;
-                        }
-
-                        visited[k] = false;
-                        visited[l] = false;
-                    }
+                    if(i == j || i == k || j == k) continue;
+                    ans = Math.min(ans, diff(i, j, k));
                 }
-
-                visited[i] = false;
-                visited[j] = false;
             }
         }
 
-        if(!isDiff) ans = -1;
+        if(ans == 100) ans = -1;
         System.out.print(ans);
+    }
+
+    private static int diff(int i, int j, int k) {
+        int sum1 = persons[i];
+        int sum2 = persons[j] + persons[k];
+        int sum3 = totalSum - (sum2 + sum1);
+
+        if(sum1 != sum2 && sum1 != sum3 && sum2 != sum3) 
+            return Math.max(Math.max(sum1, sum2), sum3) - Math.min(Math.min(sum1, sum2), sum3);
+
+        return 100;
     }
 }
