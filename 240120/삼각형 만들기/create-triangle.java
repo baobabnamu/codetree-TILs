2a99e2fa60df10aaa9b3c3fba2e93312ea30a9d6
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-01-20 15:37:56 +0900

    Title: 삼각형 만들기, Time: 112ms, Memory: 9MB, Status: Passed - CodeTree

--- 240120/삼각형 만들기/create-triangle.java
+++ 240120/삼각형 만들기/create-triangle.java
@@ -19,27 +19,22 @@ public class Main {
         }
 
         int ans = 0;
-        for(int i = 0; i < N; i++) {
-            for(int j = i + 1; j < N; j++) {
+        for(int i = 0; i < N - 2; i++) {
+            for(int j = i + 1; j < N - 1; j++) {
                 for(int k = j + 1; k < N; k++) {
                     int x1 = xPoints[i], y1 = yPoints[i];
                     int x2 = xPoints[j], y2 = yPoints[j];
                     int x3 = xPoints[k], y3 = yPoints[k];
 
                     if(isParalled(x1, x2, x3, y1, y2, y3)) {
-                        int temp = Math.abs(((x1 * y2) + (x2 * y3) + (x3 * y1)) - ((x2 * y1) + (x3 * y2) + (x1 * y3))) / 2;
+                        int temp = Math.abs(((x1 * y2) + (x2 * y3) + (x3 * y1)) - ((x2 * y1) + (x3 * y2) + (x1 * y3)));
                         ans = Math.max(ans, temp);
                     }
                 }
             }
         }
 
-        System.out.print(ans * 2);
-        // 2차평면 위 N개의 점 중 3개를 골라 삼각형을 만듬
-        // 이때, 한 면은 x 축에 평행하고 다른 한 면은 y축에 평행해야 함.
-        // -> x 축 평행 조건 : x 축이 평행하려면 세 개의 점 중 1번째 점(x,y)의 y와 2번째 점(x,y)의 y가 같아야 한다.
-        // -> y 축 평행 조건 : y 축이 평행하려면 세 개의 점 중 3번째 점(x,y)의 x와 3번째 점(x,y)의 x가 같아야 한다.
-        // 그 중에서 제일 큰 넓이에 2를 곱한 값은?
+        System.out.print(ans);
     }
 
     private static boolean isParalled(int x1, int x2, int x3, int y1, int y2, int y3) {
