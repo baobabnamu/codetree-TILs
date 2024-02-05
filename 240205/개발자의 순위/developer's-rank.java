64da86ffe4776fcff73df05d33bf97b41d8c1587
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-02-05 22:58:07 +0900

    Title: 개발자의 순위, Time: 98ms, Memory: 9MB, Status: Passed - CodeTree

--- 240205/개발자의 순위/developer's-rank.java
+++ 240205/개발자의 순위/developer's-rank.java
@@ -5,29 +5,29 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stk = new StringTokenizer(br.readLine());
-        int k = Integer.parseInt(stk.nextToken());
-        int n = Integer.parseInt(stk.nextToken());
+        int K = Integer.parseInt(stk.nextToken());
+        int N = Integer.parseInt(stk.nextToken());
 
-        int[][] numArrs = new int[k][n];
+        int[][] numArrs = new int[K][N];
 
-        for(int i = 0; i < k; i++) {
+        for(int i = 0; i < K; i++) {
             stk = new StringTokenizer(br.readLine());
-            for(int j = 0; j < n; j++) {
+            for(int j = 0; j < N; j++) {
                 numArrs[i][j] = Integer.parseInt(stk.nextToken());
             }
         }
 
         int pair = 0;
-        for(int i = 1 ; i <= n ; i++){
-            for(int j = 1 ; j <= n ; j++){
+        for(int i = 1; i <= N; i++){
+            for(int j = 1; j <= N; j++){
                 if(i == j){
                     continue;
                 }
                 int cnt = 0;
-                for(int k = 0 ; k < numArrs.length ; k++){
+                for(int k = 0; k < numArrs.length; k++){
                     int i_idx = -1;
                     int j_idx = -1;
-                    for(int h = 0 ; h < numArrs[k].length ; h++){
+                    for(int h = 0; h < numArrs[k].length; h++){
                         if(numArrs[k][h] == i){
                             i_idx = h;
                         }
