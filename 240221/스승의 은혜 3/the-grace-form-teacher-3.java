3adb0c5f0020a48b811b2c65ba1c942a9f05296f
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-02-21 10:31:20 +0900

    Title: 스승의 은혜 3, Time: 207ms, Memory: 13MB, Status: Wrong Answer - CodeTree

--- 240221/스승의 은혜 3/the-grace-form-teacher-3.java
+++ 240221/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -1,50 +1,56 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.StringTokenizer;
+import java.util.Scanner;
+import java.util.stream.IntStream;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+class Student implements Comparable<Student> {
+    int price;
+    int deliveryFee;
 
-        int result = 0;
-        int student = Integer.parseInt(st.nextToken());
-        int totalMoney = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[student][2];
-
-        for (int i = 0; i < student; i++) {
-            st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(arr, Comparator.comparingInt(a -> (a[0] + a[1])));
-
-        for (int couponIdx = 0; couponIdx < student; couponIdx++) {
-            int sum = 0;
-            int ableGiveCnt = 0;
-
-            for (int i = 0; i < student; i++) {
-                if (i == couponIdx) {
-                    sum += arr[i][0] / 2 + arr[i][1];
-                } else {
-                    sum += arr[i][0] + arr[i][1];
-                }
-
-                if (sum > totalMoney) {
-                    break;
-                }
-
-                ableGiveCnt++;
-            }
-
-            result = Math.max(result, ableGiveCnt);
-        }
-
-        System.out.println(result);
+    public Student(int price, int deliveryFee) {
+        this.price = price;
+        this.deliveryFee = deliveryFee;
     }
 
+    @Override
+    public int compareTo(Student o) {
+        return (this.price / 2 + this.deliveryFee) - (o.price / 2 + o.deliveryFee);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int b = sc.nextInt();
+        Student[] s = new Student[n];
+		int max = 0;
+
+        for(int i = 0; i < n; i++) {
+            s[i] = new Student(sc.nextInt(), sc.nextInt());
+        }
+
+		// 가격과 배송비의 합으로 정렬
+        Arrays.sort(s);
+
+		for(int i = 0; i < n; i++){
+			int sum = 0;
+			int count = 0;
+			for(int j = 0; j < n; j++){
+				int temp = 0;
+				if(i == j){
+					temp = (s[j].price / 2) + s[j].deliveryFee;
+				}else{
+					temp = s[j].price + s[j].deliveryFee;
+				}
+				if(sum + temp <= b){
+					sum += temp;
+					count++;
+				}
+			}
+			max = Math.max(max, count);
+		}
+
+		System.out.println(max);
+    }
 }
