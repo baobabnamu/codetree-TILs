94fa15a921461caa3885e9b4b7b33d1503f65a94
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-02-21 10:49:49 +0900

    Title: 스승의 은혜 3, Time: 490ms, Memory: 21MB, Status: Passed - CodeTree

--- 240221/스승의 은혜 3/the-grace-form-teacher-3.java
+++ 240221/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -1,53 +1,43 @@
-import java.util.Scanner;
 import java.util.Arrays;
-
-class Student implements Comparable<Student> {
-    int price;
-    int devlieryFee;
-
-    Student(int price, int devlieryFee) {
-        this.price = price;
-        this.devlieryFee = devlieryFee;
-    }
-
-    @Override
-    public int compareTo(Student o) {
-        return (this.price + this.devlieryFee) - (o.price + o.devlieryFee);
-    }
-}
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // 입력
-        int n = sc.nextInt();
-        int b = sc.nextInt();
-        Student[] s = new Student[n];
-        for(int i = 0; i < n; i++) {
-            s[i] = new Student(sc.nextInt(), sc.nextInt());
-        }
+		int n = sc.nextInt();
+		int b = sc.nextInt();
+		int[][] hopeProduct = new int[n][2];
+		int max = 0;
 
-        int ans = 0;
-        // 한 명의 학생에 선물 쿠폰을 쓸 때 선물 가능한 학생의 최대 명수를 구합니다.
-        for(int i = 0; i < n; i++) {
-            int sum = 0;
-            int student = 0;
-            for(int j = 0; j < n; j++) {
-                int temp = 0;
-                if(i == j) {
-                    temp = ((s[j].price / 2) + s[j].devlieryFee);
-                } else {
-                    temp = (s[j].price  + s[j].devlieryFee);
-                }
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < 2; j++){
+				hopeProduct[i][j] = sc.nextInt();
+			}
+		}
 
-                if(sum + temp <= b) {
-                    sum += temp;
-                    student++;
-                }
-            }
-            ans = Math.max(ans, student);
-        }
+		// 가격과 배송비의 합으로 정렬
+		Arrays.sort(hopeProduct, Comparator.comparingInt(a -> IntStream.of(a).sum()));
 
-        System.out.print(ans);   
+		for(int i = 0; i < n; i++){
+			int sum = 0;
+			int count = 0;
+			for(int j = 0; j < n; j++){
+				int temp = 0;
+				if(i == j){
+					temp = hopeProduct[j][0] / 2 + hopeProduct[j][1];
+				}else{
+					temp = hopeProduct[j][0] + hopeProduct[j][1];
+				}
+				if(sum + temp <= b){
+					sum += temp;
+					count++;
+				}
+			}
+			max = Math.max(max, count);
+		}
+
+		System.out.println(max);
     }
 }
