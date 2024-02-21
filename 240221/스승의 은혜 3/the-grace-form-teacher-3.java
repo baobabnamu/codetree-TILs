a323176310da1bff8bd1c4c7bf3e25ce841c35bb
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-02-21 10:57:48 +0900

    Title: 스승의 은혜 3, Time: 309ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 240221/스승의 은혜 3/the-grace-form-teacher-3.java
+++ 240221/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -1,43 +1,42 @@
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Scanner;
-import java.util.stream.IntStream;
+import java.util.*;
+import java.util.stream.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int b = sc.nextInt();
-		int[][] hopeProduct = new int[n][2];
-		int max = 0;
+        int n = sc.nextInt();
+        int b = sc.nextInt();
+        int[][] hopeProduct = new int[n][b];
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < 2; j++) {
+                hopeProduct[i][j] = sc.nextInt();
+            }
+        }
 
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < 2; j++){
-				hopeProduct[i][j] = sc.nextInt();
-			}
-		}
+        Arrays.sort(hopeProduct, Comparator.comparingInt(a -> IntStream.of(a).sum()));
 
-		// 가격과 배송비의 합으로 정렬
-		Arrays.sort(hopeProduct, Comparator.comparingInt(a -> IntStream.of(a).sum()));
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            int sum = 0;
+            int student = 0;
+            for(int j = 0; j < n; j++) {
+                int tmp = 0;
+                if(i == j) {
+                    tmp = (hopeProduct[j][0] / 2) + hopeProduct[j][1];
+                } else {
+                    tmp = hopeProduct[j][0] + hopeProduct[j][1];
+                }
 
-		for(int i = 0; i < n; i++){
-			int sum = 0;
-			int count = 0;
-			for(int j = 0; j < n; j++){
-				int temp = 0;
-				if(i == j){
-					temp = hopeProduct[j][0] / 2 + hopeProduct[j][1];
-				}else{
-					temp = hopeProduct[j][0] + hopeProduct[j][1];
-				}
-				if(sum + temp <= b){
-					sum += temp;
-					count++;
-				}
-			}
-			max = Math.max(max, count);
-		}
+                if(sum + tmp <= b) {
+                    student++;
+                    sum += tmp;
+                } else {
+                    break;
+                }
+            }
+            ans = Math.max(ans, student);
+        }
 
-		System.out.println(max);
+        System.out.print(ans);
     }
 }
