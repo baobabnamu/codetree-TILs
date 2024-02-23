c6975ba81734dcd3777ecf8f370f02b204af4051
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-02-23 20:53:53 +0900

    Title: 등차수열, Time: 203ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240223/등차수열/arithmetic-sequence.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static int[] nums;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        nums = new int[n];
+
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        int start = 101;
+        int end = 0;
+        for(int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(stk.nextToken());
+            start = Math.min(start, nums[i]);
+            end = Math.max(end, nums[i]);
+        }
+        
+        int ans = 0;
+        for(int k = start; k <= end; k++) {
+            int res = 0;
+            for(int i = 0; i < nums.length; i++) {
+                for(int j = 0; j < nums.length; j++) {
+                    if(i == j) continue;
+
+                    int a1 = nums[i];
+                    int a2 = nums[j];
+
+                    if(isAp(a1, k, a2)) {
+                        res++;
+                    }
+                }
+            }
+            ans = Math.max(ans, res);
+        }
+
+        System.out.print(ans / 2); // 중복 수열 제거
+    }
+
+    private static boolean isAp(int a1, int k, int a2) {
+        return (a2 - k) == (k - a1);
+    }
+}
