c60ee5a2720ced8a3530fec08995ac5e44b7a5ae
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-02-28 17:46:29 +0900

    Title: 3개의 선 2, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 240228/3개의 선 2/three-lines-2.java
+++ 240228/3개의 선 2/three-lines-2.java
@@ -1,36 +1,79 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-    public static final int MAX_X = 10;
-    public static final int MAX_N = 20;
-    
-    public static int n;
-    public static int[] x = new int[MAX_N];
-    public static int[] y = new int[MAX_N];
+    private static final int MAX_L = 10;
+
+    private static int[][] space;
+    private static int n;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
+        space = new int[MAX_L + 1][MAX_L + 1];
 
         for(int i = 0; i < n; i++) {
-            x[i] = sc.nextInt();
-            y[i] = sc.nextInt();
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            space[x][y] = 1;
         }
 
         int ans = 0;
-
-        for(int i = 0; i <= MAX_X; i++)
-            for(int j = 0; j <= MAX_X; j++)
-                for(int k = 0; k <= MAX_X; k++) {
-                    boolean success = false;
-                    for(int l = 0; l < n; l++) {
-                        if(x[l] == i || x[l] == j || x[l] == k) ans = 1;
-                        else if(x[l] == i || x[l] == j || y[l] == k) ans = 1;
-                        else if(x[l] == i || y[l] == j || y[l] == k) ans = 1;
-                        else ans = 1;
+        for(int i = 0; i <= MAX_L; i++) {
+            for(int j = i + 1; j <= MAX_L; j++) {
+                for(int k = j + 1; k <= MAX_L; k++) {
+                    ans = drawThreeLines(i, j, k);
+                    if(ans == 1) {
+                        System.out.print("1");
+                        return;
                     }
                 }
+            }
+        }
 
-        System.out.print(ans);
+        System.out.print("0");
+    }
+
+    private static int drawThreeLines(int line1, int line2, int line3) {
+        // 0: 가로, 1: 세로
+        int[][] dirs = {
+                {0, 0, 0},
+                {0, 0, 1},
+                {0, 1, 0},
+                {0, 1, 1},
+                {1, 0, 0},
+                {1, 0, 1},
+                {1, 1, 0},
+                {1, 1, 1}
+            };
+
+        for(int i = 0; i < 8; i++) {
+            int dir1 = dirs[i][0];
+            int dir2 = dirs[i][1];
+            int dir3 = dirs[i][2];
+
+            int cnt = 0;
+            cnt += drawLine(line1, dir1);
+            cnt += drawLine(line2, dir2);
+            cnt += drawLine(line3, dir3);
+            if(cnt == n) return 1;
+        }
+
+        return 0;
+    }
+
+    private static int drawLine(int line, int dir) {
+        int cnt = 0;
+
+        if(dir == 0) {
+            for(int i = 0; i <= MAX_L; i++) {
+                if(space[line][i] == 1) cnt++;
+            }
+        } else {
+            for(int i = 0; i <= MAX_L; i++) {
+                if(space[i][line] == 1) cnt++;
+            }
+        }
+
+        return cnt;
     }
 }
