eeb23cdfc628a495bd8d9c48d89cf83bb1d8de4b
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-02-28 17:46:42 +0900

    Title: 3개의 선 2, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240228/3개의 선 2/three-lines-2.java
+++ 240228/3개의 선 2/three-lines-2.java
@@ -19,8 +19,8 @@ public class Main {
 
         int ans = 0;
         for(int i = 0; i <= MAX_L; i++) {
-            for(int j = i + 1; j <= MAX_L; j++) {
-                for(int k = j + 1; k <= MAX_L; k++) {
+            for(int j = 0; j <= MAX_L; j++) {
+                for(int k = 0; k <= MAX_L; k++) {
                     ans = drawThreeLines(i, j, k);
                     if(ans == 1) {
                         System.out.print("1");
