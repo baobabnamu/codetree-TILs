c9ef5d753c077db6399e253793225d23b9e65e58
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-03-06 09:26:53 +0900

    Title: 숫자들의 최대 차, Time: 227ms, Memory: 12MB, Status: Passed - CodeTree

--- 240306/숫자들의 최대 차/maximum-difference-in-numbers.java
+++ 240306/숫자들의 최대 차/maximum-difference-in-numbers.java
@@ -1,56 +1,24 @@
 import java.util.*;
+
 public class Main {
-    private final static int MAX_N = 1000;
-    private final static int MAX_K = 10000;
-    private final static int INT_MAX = Integer.MAX_VALUE;
-    private final static int INT_MIN = Integer.MIN_VALUE;
-
-    private static int n, k;
-    private static int[] nums;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        k = sc.nextInt();
-        nums = new int[n];
+        int n = sc.nextInt(); int k = sc.nextInt();
+        int[] nums = new int[n];
+        for(int i = 0; i < n; i++) nums[i] = sc.nextInt();
 
-        // 1. 값 입력 받기
-        int minNum = INT_MAX;
-        int maxNum = INT_MIN;
-        for(int i = 0; i < n; i++) {
-            nums[i] = sc.nextInt();
-            minNum = Math.min(minNum, nums[i]);
-            maxNum = Math.max(maxNum, nums[i]);
-        }
-        
-        // 2. 완탐
+        Arrays.sort(nums);
+
         int ans = 0;
         for(int i = 0; i < n; i++) {
-            int cnt = calcCnt(i);
+            int cnt = 1;
+            for(int j = i + 1; j < n; j++) {
+                if(nums[j] - nums[i] <= k) 
+                   cnt++;
+            }
             ans = Math.max(ans, cnt);
         }
-        // System.out.println(calcCnt(4));
 
         System.out.print(ans);
     }
-
-    private static int calcCnt(int index) {
-        int cnt = 0;
-        int minNum = nums[index];
-        int maxNum = nums[index];
-
-        for(int i = 0; i < n; i++) {
-            
-            // System.out.println("minNum : " + minNum + ", nums[i] : " + nums[i] + ", result : " + Math.abs(minNum - nums[i]) + ", k : " + k);
-            if(Math.abs(nums[i] - minNum) <= k && maxNum - minNum <= k) {
-                cnt++;
-                minNum = Math.min(minNum, nums[i]);
-                maxNum = Math.max(maxNum, nums[i]);
-            }
-            // System.out.println("cnt : " + cnt);
-        }
-
-        return cnt;
-    }
-
 }
