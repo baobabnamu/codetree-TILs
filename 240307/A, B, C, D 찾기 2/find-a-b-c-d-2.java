5952dae6783899d2a76a167b91c215a6a250b19e
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-03-07 20:11:47 +0900

    Title: A, B, C, D 찾기 2, Time: 249ms, Memory: 20MB, Status: Passed - CodeTree

--- 240307/A, B, C, D 찾기 2/find-a-b-c-d-2.java
+++ 240307/A, B, C, D 찾기 2/find-a-b-c-d-2.java
@@ -2,23 +2,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        // A = 3, B = 3, C = 4, D = 7인 경우
-        // A, B, C, D, A + B, B + C, C + D, D + A, A + C, B + D, A + B + C, A + B + D, A + C + D, B + C + D, A + B + C + D를 나열
-        // 3, 3, 4, 7, 6, 7, 11, 10, 7, 10, 10, 13, 14, 14, 17이 되므로 입력으로 주어진 set과 동일합니다.
-
-        // 1. 15개의 정수를 입력 받고 정렬하기
-        // - 3 3 4 6 7 / 7 7 10 10 10 / 11 13 14 14 17
-        // 2. 5중 for문
-            // 2-1) a : 1 ~ 40
-            // 입력받은 배열과 비교할 배열 (15) 만들기
-                // 2-2) b : 1 ~ 40
-                    // 2-3) c : 1 ~ 40
-                        // 2-4) d : 1 ~ 40
-                        // 15개 경우의 수를 일일이 더하기
-                        // 그 후 배열 정렬
-                        
-                        // 2-5) 두 배열이 같다면
-
         Scanner sc = new Scanner(System.in);
         int[] baseNums = new int[15];
         for(int i = 0; i < 15; i++) baseNums[i] = sc.nextInt();
