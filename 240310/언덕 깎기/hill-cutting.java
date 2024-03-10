356bc829fd8e54b15a08991a8797dc38c8f06694
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-03-10 13:49:33 +0900

    Title: 언덕 깎기, Time: 263ms, Memory: 13MB, Status: Passed - CodeTree

--- 240310/언덕 깎기/hill-cutting.java
+++ 240310/언덕 깎기/hill-cutting.java
@@ -19,12 +19,6 @@ public class Main {
         for(int i = 0; i < n; i++) arr[i] = sc.nextInt();
         Arrays.sort(arr);
 
-        // 알고리즘
-        // 1. 2중 for문 
-        // 1) i : 제일 낮은 언덕, 제일 높은 언덕의 조합을 반복
-            // 2) j : arr 순회하면서 언덕을 제일 낮은 언덕으로 만드는 비용과 제일 높은 언덕으로 만드는 비용 중 더 작은 것을 기준으로 cost에 누적함
-        // 총 누적된 비용 적은 값으로 갱신
-
         int ans = INT_MAX;
         for(int i = 0; i <= MAX_H; i++) {
             minVal = i;
