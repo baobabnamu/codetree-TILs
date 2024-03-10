aa7d7ba3e2dcf9f17c7bc29feaf033f724904004
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-03-10 12:40:36 +0900

    Title: 이상한 폭탄 3, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

--- 240310/이상한 폭탄 3/strange-bomb-3.java
+++ 240310/이상한 폭탄 3/strange-bomb-3.java
@@ -1,53 +1,47 @@
+import java.io.*;
 import java.util.*;
+
 public class Main {
-    private final static int MAX_NUMBER = 1000000;
-    private final static int MAX_N = 100;
 
-    private static int n, k, cnt;
-    private static int[] boombs;
+    static final int MAX_N = 100;
+    static int N, K;
+    static int[] bombs;
 
-    public static void main(String[] args) {
-        // 폭탄 N개가 주어짐
-        // 정수 값이 같은 폭탄끼리의 거리가 K 안에 있으면 폭발
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt(); k = sc.nextInt();
-        boombs = new int[n];
-        for(int i = 0; i < n; i++) boombs[i] = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        bombs = new int[MAX_N + 1];
 
-        // 아이디어 : 완탐으로 하나하나 터지는 걸 계산해보기
-        // 1) i : 폭탄 배열을 순회
-        // ans : Math.max(ans, 7번이 터질 때의 터지는 폭탄 개수를 구하는 함수 호출)
-        // TC :
-        // 0이 1개만 들어오는 경우
-        // 폭탄의 번호가 최댓값인 경우
-        // 연달아 폭탄이 터지는 경우
-        int ans = 0;
-        int max = 1;
-        for(int i = 0; i < n; i++) {
-            boolean[] useBoombs = new boolean[n];
-            cnt = 0;
-            coutingBomb(boombs[i], i, useBoombs);
-            // System.out.printf("boomb[%d] : %d, cnt : %d\n" , i, boombs[i], cnt);
-            if(max < cnt) {
-                ans = boombs[i];
-                max = cnt;
-            } 
-            else if(max == cnt && ans < i && ans != 0) {
-                ans = boombs[i];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < N; i++) {
+            bombs[i] = Integer.parseInt(br.readLine());
+        }
+
+        int maxBomb = 0;
+        int maxFindBombCount = 0;
+
+        for (int curr = 0; curr < N; curr++) {
+            int currBomb = bombs[curr];
+            int findBombCount = 0;
+            for (int left = curr - K; left < curr; left++) {
+                if (left < 0) continue;
+                if (currBomb == bombs[left]) findBombCount++;
+            }
+            for (int right = curr + 1; right < curr + K; right++) {
+                if (right >= N) continue;
+                if (currBomb == bombs[right]) findBombCount++;
+            }
+
+            if (findBombCount != 0 && (maxFindBombCount < findBombCount ||
+                    (maxFindBombCount == findBombCount && maxBomb < currBomb))) {
+                maxBomb = currBomb;
+                maxFindBombCount = findBombCount;
             }
         }
-        System.out.print(ans);
-    }
 
-    // 터진 곳을 기점으로 더 순회해야 됨.
-    private static void coutingBomb(int boomb, int index, boolean[] useBoombs) { // 2, 1, boolean[]
-        for(int i = index; i <= i + k; i++) {
-            if(i >= n) break;
-            if(boombs[i] == boomb && !useBoombs[i]) {
-                cnt++;
-                useBoombs[i] = true;
-                coutingBomb(boombs[i] + 1, index + 1, useBoombs);
-            }
-        }
+        System.out.println(maxBomb);
     }
 }
