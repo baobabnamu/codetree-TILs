7b90abecdd8f9bc80b40f8f04180539fb0ea8fbd
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-03-10 12:06:54 +0900

    Title: 이상한 폭탄 3, Time: 149ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240310/이상한 폭탄 3/strange-bomb-3.java
@@ -0,0 +1,49 @@
+import java.util.*;
+public class Main {
+    private final static int MAX_NUMBER = 1000000;
+    private final static int MAX_N = 100;
+
+    private static int n, k, cnt;
+    private static int[] boombs;
+
+    public static void main(String[] args) {
+        // 폭탄 N개가 주어짐
+        // 정수 값이 같은 폭탄끼리의 거리가 K 안에 있으면 폭발
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt(); k = sc.nextInt();
+        boombs = new int[n];
+        for(int i = 0; i < n; i++) boombs[i] = sc.nextInt();
+
+        // 아이디어 : 완탐으로 하나하나 터지는 걸 계산해보기
+        // 1) i : 폭탄 배열을 순회
+        // ans : Math.max(ans, 7번이 터질 때의 터지는 폭탄 개수를 구하는 함수 호출)
+        int ans = 0;
+        int max = 0;
+        for(int i = 0; i < n; i++) {
+            boolean[] useBoombs = new boolean[n];
+            cnt = 0;
+            coutingBomb(boombs[i], i, useBoombs);
+            // System.out.printf("boomb[%d] : %d, cnt : %d\n" , i, boombs[i], cnt);
+            if(max < cnt) {
+                ans = boombs[i];
+                max = cnt;
+            } 
+            else if(max == cnt && ans < i) {
+                ans = boombs[i];
+            }
+        }
+        System.out.print(ans);
+    }
+
+    // 터진 곳을 기점으로 더 순회해야 됨.
+    private static void coutingBomb(int boomb, int index, boolean[] useBoombs) { // 2, 1, boolean[]
+        for(int i = index; i <= i + k; i++) {
+            if(i >= n) break;
+            if(boombs[i] == boomb && !useBoombs[i]) {
+                cnt++;
+                useBoombs[i] = true;
+                coutingBomb(boombs[i] + 1, index + 1, useBoombs);
+            }
+        }
+    }
+}
