86e0a122780ec7db8b1c9685ed5926b6b8831c5b
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-03-11 15:29:57 +0900

    Title: 구간 잘 나누기, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240311/구간 잘 나누기/divide-sections-well.java
+++ 240311/구간 잘 나누기/divide-sections-well.java
@@ -1,46 +1,43 @@
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		// 여기에 코드를 작성해주세요.
-		Scanner sc = new Scanner(System.in);
+    private static final int MAX_N = 100;
+    private static final int MAX_NUMBER = MAX_N * MAX_N;
 
-		int N = sc.nextInt();
-		int M = sc.nextInt();
+    private static int n, m;
+    private static int[] nums = new int[MAX_N];
 
-		int total = 0;
-		int[] list = new int[N];
-		for (int i = 0; i < N; i++) {
-			list[i] = sc.nextInt();
-			total += list[i];
-		}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt(); m = sc.nextInt();
+        for(int i = 0; i < n; i++) nums[i] = sc.nextInt();
 
-		int ans = Integer.MAX_VALUE;
+        int ans = MAX_NUMBER;
+        for(int i = 1; i <= MAX_NUMBER; i++) {
+            boolean possible = true;
+            int section = 1;
+            int sum = 0;
 
-		for (int i = 0; i <= total; i++) {
-			int[] sumList = new int[M];
+            for(int j = 0; j < n; j++) {
+                if(nums[j] > i) {
+                    possible = false;
+                    break;
+                }
 
-			int idx = 0;
+                if(nums[j] + sum > i) {
+                    section++;
+                    sum = 0;
+                }
 
-			for (int k = 0; k < N; k++) {
-				if (idx == M) {
-					sumList[idx - 1] += list[k];
-				} else if (sumList[idx] + list[k] > i) {
-					k--;
-					idx++;
-				} else {
-					sumList[idx] += list[k];
-				}
-			}
+                sum += nums[j];
+            }
 
-			int max = 0;
-			for (int k = 0; k < sumList.length; k++) {
-				max = Math.max(max, sumList[k]);
-			}
+            if(possible && section <= m) {
+                ans = Math.min(ans, sum);
+                break;
+            }
+        }
 
-			ans = Math.min(ans, max);
-		}
-
-		System.out.print(ans);
-	}
+        System.out.print(ans);
+    }
 }
