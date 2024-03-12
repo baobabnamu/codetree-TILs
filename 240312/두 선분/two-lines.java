f9dc11e8c2cc7d411cd82a04c3f6560c534867e5
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-03-12 16:19:33 +0900

    Title: 두 선분, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 240312/두 선분/two-lines.java
+++ 240312/두 선분/two-lines.java
@@ -6,7 +6,7 @@ public class Main {
         int x1 = sc.nextInt(); int x2 = sc.nextInt(); int x3 = sc.nextInt(); int x4 = sc.nextInt();
 
         if(x2 < x3 || x4 < x1) {
-            System.out.print("not intersecting");
+            System.out.print("nonintersecting");
         } else {
             System.out.print("intersecting");
         }
