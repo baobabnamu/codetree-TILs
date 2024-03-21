2559ec80e5b36578476a22447571ec433bab6ab0
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-03-21 11:06:41 +0900

    Title: 최대 H 점수 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240321/최대 H 점수 2/maximum-h-score-2.java
+++ 240321/최대 H 점수 2/maximum-h-score-2.java
@@ -1,17 +1,14 @@
-package 코트.구현력.완전탐색.기준새로설정.최대H점수2;
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
-
     static final int MIN_N = 1;
     static final int MAX_N = 100;
     static int[] arr;
     static int N, L;
 
-    public static void main(String[] args) throws IOException {
-        arr = new int[MAX_N];
+    public static void main(String[] args) {
+       arr = new int[MAX_N];
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
